1422fb1511a559580afa8e369f948a2658f3ac22
ahmed <ahmadabdullah4919@gmail.com>
2023-01-29 16:23:49 +0500

    :boom: Initialize

---
+++ src/vars.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    let name = "Brad";
+    let mut age = 37;
+    println!("My name is {} and I am {}", name, age);
+    age = 39;
+    println!("My name is {} and I am {}", name, age);
+}
