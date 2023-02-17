487b08bd4dd708f2769b8b3e717e20031a678ad6
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 15:53:42 +0500

    Move v Deep Copy

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -12,4 +12,21 @@ fn main() {
         println!("Country: {country}");
     }
     // println!("Country: {country}"); // Error: country is out of scope
+
+    let s1 = String::from("Hello");
+    let s2 = s1;
+
+    // s1 is moved to s2 and can no longer be used
+    // Bcz both s1 and s2 are pointing to same memory 
+    // location in heap, as it creates shallow copy, in
+    // rust it is called as move
+
+    // println!("s1: {s1}"); // Error: s1 is moved to s2
+
+    // Deep Copy
+    let s3 = String::from("Hello");
+    let s4 = s3.clone();
+    println!("s3: {s3}");
+    println!("s4: {s4}");
+
 }
