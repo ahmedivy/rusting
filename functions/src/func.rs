f1cd86f427380256b8ec244b3345d218d2059e5a
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 13:40:58 +0500

    Restructure

---
+++ functions/src/func.rs
@@ -0,0 +1,18 @@
+pub fn run() {
+    println!("Hello, world!");
+    simple_function();
+    one_arg_function(5);
+    multiple_arg_function(4, 'm');
+}
+
+fn simple_function() {
+    println!("Another function.");
+}
+
+fn one_arg_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn multiple_arg_function(length: i32, unit: char) {
+    print!("The length is {length} {unit}.")
+}
--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,18 +1,5 @@
+mod func;
+
 fn main() {
-    println!("Hello, world!");
-    simple_function();
-    one_arg_function(5);
-    multiple_arg_function(4, 'm');
-}
-
-fn simple_function() {
-    println!("Another function.");
-}
-
-fn one_arg_function(x: i32) {
-    println!("The value of x is: {x}");
-}
-
-fn multiple_arg_function(length: i32, unit: char) {
-    print!("The length is {length} {unit}.")
+    func::run();
 }
