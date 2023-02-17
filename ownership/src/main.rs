1229173affe5b7bc3c6d95d6d0df07f11af648ac
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 15:33:08 +0500

    Heap v Stack

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // On Stack
+    let name = "Ahmed";
+    println!("Name: {name}");
+
+    // On Heap (mutable)
+    let city = String::from("Lahore");
+    println!("City: {city}");
+
+    {
+        let country = String::from("Pakistan");
+        println!("Country: {country}");
+    }
+    // println!("Country: {country}"); // Error: country is out of scope
 }
