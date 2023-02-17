dfd17bd8c6c00e3f2e0c953254d786687514084d
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 20:18:03 +0500

    Slice Refs.

--- slice-type/src/main.rs
+++ slice-type/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{} {}", hello, world);
+
+    let s = String::from("hello");
+    // let slice = &s[0..2];
+    // let slice = &s[..2];
+    // let slice = &s[3..];
+    let slice = &s[..];
+
+    println!("{slice}");
 }
