517699f7e1ac07b04761e8f078663aecd9834de1
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 13:53:21 +0500

    Return Values

--- functions/src/func.rs
+++ functions/src/func.rs
@@ -3,6 +3,8 @@ pub fn run() {
     simple_function();
     one_arg_function(5);
     multiple_arg_function(4, 'm');
+    let sum = sum(5, 6);
+    println!("The sum is: {sum}.");
 }
 
 fn simple_function() {
@@ -14,5 +16,9 @@ fn one_arg_function(x: i32) {
 }
 
 fn multiple_arg_function(length: i32, unit: char) {
-    print!("The length is {length} {unit}.")
+    println!("The length is {length} {unit}.")
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y // No return keyword, No ; at the end
 }
