c849f8fbbdcec45d5f8fad314c16d839624b13a1
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 19:23:06 +0500

    Mutable Ref.

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -2,8 +2,18 @@ fn main() {
     let s = String::from("hello");
     let len = calculate_length(&s);
     println!("The length of '{}' is {}.", s, len);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("The value of s is '{s}'.");
 }
 
 fn calculate_length(s: &String) -> usize {
+    // s.push_str(", world!"); // Can not do this because s is immutable
     s.len()
+}
+
+// Mutable References
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
