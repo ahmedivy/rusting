9fa68ca1551dfaacce00dc6a37cbe582ddcd47dc
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 17:21:21 +0500

    Return Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     println!("s2: {s2}");
 
     // s1 is moved to s2 and can no longer be used
-    // Bcz both s1 and s2 are pointing to same memory 
+    // Bcz both s1 and s2 are pointing to same memory
     // location in heap, as it creates shallow copy, in
     // rust it is called as move
 
@@ -31,25 +31,57 @@ fn main() {
     println!("s4: {s4}");
 
     // Ownership and Functions
-    let s = String::from("hello");  // s comes into scope
+    let s = String::from("hello"); // s comes into scope
 
-    takes_ownership(s);             // s's value moves into the function...
-                                                // ... and so is no longer valid here
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
 
-    let x = 5;                      // x comes into scope
+    let x = 5; // x comes into scope
 
-    makes_copy(x);                  // x would move into the function,
-                                                // but i32 is Copy, so it's okay to still
-                                                // use x afterward
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
 
+    let s1 = gives_ownership(); // gives_ownership moves its return
+                                // value into s1
+
+    let s2 = String::from("hello"); // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2); // s2 is moved into
+                                       // takes_and_gives_back, which also
+                                       // moves its return value into s3
+
+    println!("s1: {s1}");
+    println!("s3: {s3}");
 } // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens.
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
     println!("{}", some_string);
 } // Here, some_string goes out of scope and `drop` is called. The backing
   // memory is freed.
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into scope
+
+    a_string // a_string is returned and moves out to the calling function
+}
