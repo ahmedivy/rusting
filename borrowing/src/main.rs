d143860a1e4a12abd2bd2551ca16724b5c21d890
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 17:36:15 +0500

    Simple Ref.

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello");
+    let len = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
