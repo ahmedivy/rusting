c87b76f9550cbc98c57879d9fd684245d95ae044
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 12:22:13 +0500

    Primitive Types

---
+++ variables/src/primitive.rs
@@ -0,0 +1,28 @@
+fn main() {
+
+    // Int
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // Float
+    let mut y = 0.2;
+    println!("The value of y is: {y}");
+    y = 2.56;
+    println!("The value of y is: {y}");
+    
+    // Bool
+    let mut a = true;
+    println!("The value of a is: {a}");
+    a = false;
+    println!("The value of a is: {a}");
+
+    // Char
+    let mut b = 'a';
+    println!("The value of b is: {b}");
+    b = 'b';
+    println!("The value of b is: {b}");
+    b = '💕';
+    println!("The value of b is: {b}");
+}
