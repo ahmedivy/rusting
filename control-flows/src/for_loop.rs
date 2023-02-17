b4b93914ddb4f26125bfc4ef99b2e655238188b2
ahmed <ahmadabdullah4919@gmail.com>
2023-02-17 14:57:15 +0500

    Simple For Loop

---
+++ control-flows/src/for_loop.rs
@@ -0,0 +1,6 @@
+pub fn run() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("The value is: {element}");
+    }
+}
--- control-flows/src/loop_labels.rs
+++ control-flows/src/loop_labels.rs
@@ -1,11 +1,14 @@
 pub fn run() {
-    'outer: loop {
-        println!("Outer loop.");
-        'inner: loop {
-            println!("Inner loop.");
-            break 'outer;
-        }
-        println!("This will never print.");
-    }
-    println!("Exited outer loop.");
+
+    // Commented out because it shows warnings
+
+    // 'outer: loop {
+    //     println!("Outer loop.");
+    //     'inner: loop {
+    //         println!("Inner loop.");
+    //         break 'outer;
+    //     }
+    //     println!("This will never print.");
+    // }
+    // println!("Exited outer loop.");
 }
