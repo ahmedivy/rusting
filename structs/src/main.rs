caf74715ce030d43526d8113968272433aa0afc0
ahmed <ahmadabdullah4919@gmail.com>
2023-02-18 16:02:31 +0500

    Tuple Structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,10 @@ struct User {
     active: bool,
 }
 
+// Tuple Structs
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
     let mut ahmed = build_user(String::from("ahmad@mail.com"), String::from("ahmed"));
 
@@ -17,6 +21,13 @@ fn main() {
         (if ahmed.active { "Active" } else { "Inactive" })
     );
     println!("{} signed in {} times", ahmed.username, ahmed.sign_in_count);
+
+    // Tuple Structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("Black is ({}, {}, {})", black.0, black.1, black.2);
+    println!("Origin is ({}, {}, {})", origin.0, origin.1, origin.2);
 }
 
 fn build_user(email: String, username: String) -> User {
