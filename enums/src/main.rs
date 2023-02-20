ac6005ebb2ffb41f7579a431619da9b405690fad
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 19:58:47 +0500

    Warning Fix

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum IpAddrKind {
     V4(u8, u8, u8, u8),
     V6(String),
@@ -12,4 +13,7 @@ fn main() {
     // let home = IpAddrKind::V4(String::from("127.0.0.1"));
     let home = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"));
+
+    println!("home: {:?}", home);
+    println!("loopback: {:?}", loopback);
 }
