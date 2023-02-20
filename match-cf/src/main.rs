186cf5fcf9bce036077d85b28675f8470ac661af
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 21:25:32 +0500

    Excl. Some Arms

--- match-cf/src/main.rs
+++ match-cf/src/main.rs
@@ -18,6 +18,30 @@ fn main() {
     value_in_cents(Coin::Dime);
     value_in_cents(Coin::Quarter(UsState::Alaska));
     value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    // Match with Option<T>
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("five: {:?}, six: {:?}, none: {:?}", five, six, none);
+
+    // Match with _
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
