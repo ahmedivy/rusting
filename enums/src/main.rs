fe4061a9ba370e3196ce432469f4820d47b7186d
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 19:53:09 +0500

    Enum Vers. 2

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,21 +1,15 @@
 #[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    println!("four: {:?}, six: {:?}", four, six);
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 }
