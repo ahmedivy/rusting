8f8257194170ab2bab9f15e9c55d398d2d831dd0
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 20:12:33 +0500

    Create Pkg

---
+++ option-enum/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // // Rust does not have nulls, but it does
+    // // have an enum that can encode this concept.
+    // let some_number = Some(5);
+    // let some_string = Some("a string");
+    // let absent_number: Option<i32> = None;
+}
