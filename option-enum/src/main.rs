8931e3b153e5361180fa0c7bc0b91ff16ca914e4
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 20:27:42 +0500

    Option Enum v Null

--- option-enum/src/main.rs
+++ option-enum/src/main.rs
@@ -1,7 +1,15 @@
+// The Option<T> enum is defined as follows:
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+
 fn main() {
     // Rust does not have nulls, but it does
     // have an enum that can encode this concept.
     let some_number = Some(5);
     let some_string = Some("a string");
     let absent_number: Option<i32> = None;
+
+    println!("{:?} {:?} {:?}", some_number, some_string, absent_number);
 }
