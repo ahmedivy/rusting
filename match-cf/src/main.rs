29294eb800c004840806b6b975d00b1d4ff07ea1
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 20:46:19 +0500

    Match Syntax

--- match-cf/src/main.rs
+++ match-cf/src/main.rs
@@ -12,13 +12,22 @@ enum Coin {
     Quarter(UsState),
 }
 
-fn main() {}
+fn main() {
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Dime);
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+}
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
