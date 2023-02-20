3191627f70875b21da76841c1a1838914c187ff3
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 20:01:53 +0500

    Add Impl. to Enum

--- enums/src/example.rs
+++ enums/src/example.rs
@@ -1,6 +1,19 @@
+#[derive(Debug)]
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
     ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+        println!("Message: {:?}", self);
+    }
+}
+
+pub fn run() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
