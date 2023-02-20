743d52cfc0f04952e46278c2f51a24d474c546a0
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 19:54:37 +0500

    Enum Vers. 3

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 enum IpAddrKind {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
@@ -10,6 +10,7 @@ enum IpAddrKind {
 // }
 
 fn main() {
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    // let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let home = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"));
 }
