fd7d341a410641d3f7ea2cc48cd56c296a2f5755
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 20:35:30 +0500

    Coin Example

--- match-controlflow/src/main.rs
+++ match-controlflow/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn main() {}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
