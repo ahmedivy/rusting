1bf49b2080d528951a5be81c3250173a976a58c8
ahmed <ahmadabdullah4919@gmail.com>
2023-02-20 19:46:46 +0500

    Enum Syntax

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("four: {:?}, six: {:?}", four, six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
 }
