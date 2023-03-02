a1cbabfb6b53ff0af6ce4567f2a185d14956f950
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2023-03-02 23:21:12 +0500

    Update main.rs

--- option-enum/src/main.rs
+++ option-enum/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     // Rust does not have nulls, but it does
     // have an enum that can encode this concept.
     let some_number = Some(5);
-    let some_string = Some("a string");
+    let some_string = Some("A String");
     let absent_number: Option<i32> = None;
 
     println!("{:?} {:?} {:?}", some_number, some_string, absent_number);
